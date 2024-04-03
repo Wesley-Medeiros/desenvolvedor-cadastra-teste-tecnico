f3dc7e3709f87b6a4f36852a5a2768eeaaefcf96
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2024-04-03 08:56:06 -0300

    Finalização da estilização desktop

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,7 +3,20 @@ import { Product } from "./Product";
 const serverUrl = "http://localhost:5000";
 
 function main() {
-  console.log(serverUrl);
-}
+  
+    
+    document.querySelector(".dropDown").addEventListener("click", async () => {
+      document.querySelector(".dropDown").classList.toggle("show");
+    })
+    const titleSeeMore = document.querySelector(".spanDropHide")
+    const seeAllColorsContainer = document.querySelector(".hideCheckbox")
+  
+    titleSeeMore.addEventListener("click", async () => {
+      seeAllColorsContainer.classList.toggle("show")
+      titleSeeMore.classList.toggle("hidden")
+    })
+  
+  }
+  
 
 document.addEventListener("DOMContentLoaded", main);
