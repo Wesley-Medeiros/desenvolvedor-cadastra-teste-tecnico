b37c0a9e5be54502baa28a2e46dd8ed305827383
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2024-04-11 15:47:40 -0300

    feat: configuração do vercel

--- src/ts/showMore.ts
+++ src/ts/showMore.ts
@@ -5,7 +5,6 @@ import { Filter } from './typings'
 export function showMore(filter: Filter) {
 
   const changeLimitButton = document.getElementById('myBtn')
-  const hiddenButton = document.getElementById("myBtn")
       changeLimitButton.addEventListener('click', async () => {
           fetchProducts(filter, 14).then(res => {
               newObject(res.data)
